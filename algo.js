3cad51f3c9993f671df6af227cafa03d5adc385d
Samybw <samybn@yahoo.com>
2020-06-19 11:57:31 +0200

    My first algo i found the word in the phrasing

---
+++ algo.js
@@ -0,0 +1,17 @@
+function plusGrand(string) {
+    let mot = string.split(' ');
+   let longMax = 0;
+   let grandMot;
+   
+   for (let i = 0; i < mot.length; i++) {
+       if (mot[i].length > longMax) {
+           longMax = mot[i].length
+           grandMot = mot[i]
+       }
+
+      }
+      return grandMot   
+}     
+  console.log(plusGrand('Je suis le meilleur je le roi KingSamy le WolfKingBoss Samy !'));
+ 
+  
